3a43db6a69cffe0ae4cd775589315722dea152f9
Chirag <chirag0264@gmail.com>
2024-04-18 16:29:43 +0530

    merged branches

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,13 +1,12 @@
 import { Connect } from '@/hooks/Connect'
 import { ModeToggle } from '../mode-toggle'
+import logo from '../../assets/alchemix-logo.svg'
 
 const Navbar = () => {
     return (
         <>
             <div className="flex justify-between p-6">
-                <h1 className="text-3xl font-bold justify-center items-center ">
-                    Alchemix
-                </h1>
+                <img src={logo} alt="Alchemix" className="w-40" />
                 <div className="flex justify-center items-center gap-6">
                     <Connect />
                     <ModeToggle />
