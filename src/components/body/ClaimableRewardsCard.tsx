2e58ebb47e093e6fa8cb4bba404874f9a0f47aad
jenzen <ishankxjena@gmail.com>
2024-04-18 15:59:15 +0530

    added claim assets card

--- src/components/body/ClaimableRewardsCard.tsx
+++ src/components/body/ClaimableRewardsCard.tsx
@@ -7,54 +7,51 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import ClaimableRewardsTable from "./ClaimableRewardsTable";
+
+const assets = [
+  {
+    tokenName: "CRV",
+    amount: "100",
+  },
+  {
+    tokenName: "CVX",
+    amount: "900",
+  },
+  {
+    tokenName: "FXS",
+    amount: "600",
+  },
+  {
+    tokenName: "3CRV",
+    amount: "500",
+  },
+];
+
+function getTotalAssets(assets) {
+    let totalAmount = 0;
+    for (const asset of assets) {
+      totalAmount += parseInt(asset.amount);
+    }
+    return totalAmount;
+  }
 
 const ClaimableRewardsCard = () => {
   return (
     <>
       <div className="claimableRewards m-5">
-        <Card className="w-[350px]">
+        <Card className="w-[400px]">
           <CardHeader>
-            <CardTitle>Create project</CardTitle>
+            <CardTitle>Claim Assets</CardTitle>
             <CardDescription>
-              Deploy your new project in one-click.
+              Assets available to claim from AMOS
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" placeholder="Name of your project" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="framework">Framework</Label>
-                  <Select>
-                    <SelectTrigger id="framework">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">Next.js</SelectItem>
-                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                      <SelectItem value="astro">Astro</SelectItem>
-                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
-            </form>
+            <ClaimableRewardsTable toClaim={assets}/>
           </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Deploy</Button>
+          <CardFooter className="flex justify-between items-center">
+            <Button>Total Value: {getTotalAssets(assets)}</Button>
           </CardFooter>
         </Card>
       </div>
---
+++ src/components/body/ClaimableRewardsTable.tsx
@@ -0,0 +1,42 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Checkbox } from "../ui/checkbox";
+
+
+const ClaimableRewardsTable = (props) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]"></TableHead>
+          <TableHead>Claimable Tokens</TableHead>
+          <TableHead className="text-right">Balance</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {props.toClaim.map((asset) => (
+          <TableRow key={asset.tokenName}>
+            <TableCell className="font-medium">
+              <Checkbox id="asset" />
+              <label
+                htmlFor="asset"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+              </label>
+            </TableCell>
+            <TableCell className="font-medium">{asset.tokenName}</TableCell>
+            <TableCell className="text-right">{asset.amount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default ClaimableRewardsTable;
